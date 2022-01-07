e5b30cf0f1a657129c68ec389cb315922bb448a2
DoganHaluk <halukdogan78@gmail.com>
2022-01-07 23:06:19 +0100

    validation with spring

---
+++ src/main/java/be/vdab/validation/domain/Product.java
@@ -0,0 +1,30 @@
+package be.vdab.validation.domain;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+
+public class Product {
+    @PositiveOrZero
+    @Digits(integer = 7, fraction = 2)
+    private BigDecimal aankoopPrijs;
+    @PositiveOrZero
+    @Digits(integer = 7, fraction = 2)
+    private BigDecimal verkoopPrijs;
+
+    public BigDecimal getAankoopPrijs() {
+        return aankoopPrijs;
+    }
+
+    public void setAankoopPrijs(BigDecimal aankoopPrijs) {
+        this.aankoopPrijs = aankoopPrijs;
+    }
+
+    public BigDecimal getVerkoopPrijs() {
+        return verkoopPrijs;
+    }
+
+    public void setVerkoopPrijs(BigDecimal verkoopPrijs) {
+        this.verkoopPrijs = verkoopPrijs;
+    }
+}
